590375348dd4d0bf7fafb57aa597eee882f4c252
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-30 17:59:54 +0200

    [ORIGIN][MASTER][UPDATED] credit service config yml

--- src/main/java/com/mymovies/service/CreditService.java
+++ src/main/java/com/mymovies/service/CreditService.java
@@ -5,10 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class CreditService implements ICreditService {
 
 	@Value("${resource.api.url.base}")
@@ -31,9 +33,13 @@ public class CreditService implements ICreditService {
 	public CreditsDTO getAPI_Credit(String movie_id) {
 
 		CreditsDTO credits = null;
+
+		String url = BASE_URL+movie_id+"/credits"+API_KEY+Language;
+
+		LOGGER.info("@Get getAPI_Credit Service URL : " + url);
 		
 		try {
-			credits = restTemplate.getForObject(BASE_URL+movie_id+"/credits"+API_KEY+Language, CreditsDTO.class);
+			credits = restTemplate.getForObject(url, CreditsDTO.class);
 		} catch (Exception e) {
 			LOGGER.error("Unexpected Error From Service: getAPI_Credit: " + e);
 		}
